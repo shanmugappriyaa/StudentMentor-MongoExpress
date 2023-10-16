1144209fc66836662fe6e751d890d7c94438f254
shanmugappriyaa <pspri03@gmail.com>
2023-10-16 13:10:33 +0530

    env file added

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const dotenv = require('dotenv')
+dotenv.config()
 const route = require("./src/routes");
-const DB = require("./src/common/dbConfig");
 const { mongoose } = require("mongoose");
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json());
 app.use("/", route);
 
 mongoose
-  .connect(`${DB.dbUrl}/${DB.dbName}`)
+  .connect(`${process.env.dbUrl}/${process.env.dbName}`)
   .then(() => {
     app.listen(8000, () => console.log("Server is listening in port 8000"));
   })
   .catch((error) => {
-    console.log("Failed to connect Db", error, `${DB.dbUrl}/${DB.dbName}`);
+    console.log("Failed to connect Db", error, `${process.env.dbUrl}/${process.env.dbName}`);
   });
